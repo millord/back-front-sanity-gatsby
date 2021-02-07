c77a4051fede521acb60d3a2807c1dc91982853b
millord <juancarlosmillord.gmail.com>
2021-02-07 12:01:25 -0400

    first commit

---
+++ studio/schemas/sponsor.js
@@ -0,0 +1,18 @@
+// in sponsor.js
+export default {
+  name: "sponsor",
+  title: "Sponsor",
+  type: "object",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "url",
+      title: "URL",
+      type: "url",
+    },
+  ],
+};
